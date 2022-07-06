3e34086787020992295d07fa2bb31d7c1464c097
Vitaliy Lukin <mrlvs2015@gmail.com>
2022-07-06 13:38:29 +0300

    test when

--- Jenkinsfile-test.groovy
+++ Jenkinsfile-test.groovy
@@ -1,35 +1,39 @@
 pipeline {
     agent any
-    stages {
-        stage('Run  tests') {
-            environment{
-                NAME2 = "${NAME2.tokenize('/')[-1]}"
-
-            }
-            parallel {
-                stage('Run 1') {
-                    steps {
-                        sh "echo 'Env Name1 $NAME'"
-                    }
-                }
-                stage('Run 2') {
-                    steps {
-                        sh "echo 'Env NAME_CM = $NAME2'"
-                    }
-                }
-                stage('Run 3') {
-                    steps {
-                        
-                        sh "echo 'Env NAME_CM = $NAME2'"
-                    }
-                }
-            }
-        }
-        stage('Run  tests2') {
-                                steps {
-                        
-                        sh "echo 'Env NAME_CM = $NAME2'"
-                    }
-        }
+    parameters{
+        string(name: 'SEV_BRANCH', defaultValue: 'master', description: 'Branch from smtp-email-validator repo')
+        string(name: 'TESTS_JOB_NAME', defaultValue: '', description: 'The name of the job to be launched by the trigger')
+        string(name: 'PUSH_TO_TESTRAIL', defaultValue: 'yes', description: 'Parameter to push test results in testrail')
+        string(name: 'TEST_TYPE', defaultValue: 'smoke', description: 'Type of test to run')
+        string(name: 'SPECIFIED_TESTS_LIST', defaultValue: '', description: 'Test lists')
     }
+        stages {
+            stage('Build images'){
+                sh 'echo "Build images"'
+            }
+            stage('Run  tests') {
+                when{
+                    expression{ params.TEST_TYPE == 'acceptance' || params.TEST_TYPE == 'smoke' && !params.SPECIFIED_TESTS_LIST }
+                }
+                    steps {
+                        sh "echo 'Env TEST_TYPE $TEST_TYPE list - $SPECIFIED_TESTS_LIST'"
+                } 
+            }
+            stage('Run  tests 2') {
+                when{ expression{ params.SPECIFIED_TESTS_LIST }}
+                
+                    steps {
+                        sh "echo 'Env TEST_TYPE $TEST_TYPE list - $SPECIFIED_TESTS_LIST'"
+                } 
+            }
+            stage('Run  unit') {
+                when{ expression{ !params.SPECIFIED_TESTS_LIST  && params.TEST_TYPE == unit}}
+                
+                    steps {
+                        sh "echo 'Env TEST_TYPE $TEST_TYPE list - $SPECIFIED_TESTS_LIST'"
+                } 
+            }
+        }
 }
+
+
